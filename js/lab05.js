da0dc4ca93a9ead8b9793ac441e8ce151bc3b090
Angel Alvites <bicirepuestos.david01@gmail.com>
2023-03-31 15:01:41 -0500

    Version 0.0.1

---
+++ js/lab05.js
@@ -0,0 +1,13 @@
+function extraer(){
+    //obtener valores de los campos
+    let apNom = document.getElementById("nombre").value;
+    let fNac = document.getElementById("fecha").value;
+
+    console.log(apNom);
+    console.log(fNac);
+}
+
+
+function colorear() {
+    let color = document.getElementById("color").style.background = "rgb(34, 112, 185)";
+}
